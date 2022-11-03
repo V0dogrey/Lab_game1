12ad17f1b4da119ed9815d788fa1525fa56cb762
V0dogrey <CanptanVodogrey@gmail.cm>
2022-11-03 16:54:54 +0500

    make shield

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 import random
 pygame.init()
 
-FPS = 2
+FPS = 30
 screen_size = (1200, 800)
 screen = pygame.display.set_mode(screen_size)
 '''set screen resolution'''
@@ -22,11 +22,14 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 class Player:
     start_position = (screen_size[0] / 2, screen_size[1] - 150)
     ship_shape = [[25, 15], [0, - 25], [-25, 15], [0, 0]]
+    ss = 30
+    start_shield_hp = 1
     start_speed = 30
 
     def __init__(self):
         self.pos = self.start_position
         self.speed = self.start_speed
+        self.shield_hp = self.start_shield_hp
 
     def draw(self):
         dots = []
@@ -35,6 +38,9 @@ class Player:
             y_pos = a[1] + self.pos[1]
             dots.append([x_pos, y_pos])
         pygame.draw.polygon(screen, GRAY, dots)
+
+        shield_cords = (self.pos[0] - self.ss, self.pos[1] - self.ss * 2, self.ss * 2, self.ss * 4)
+        pygame.draw.ellipse(screen, BLUE, shield_cords, self.shield_hp)
 
     def move(self, move):
         self.pos = (self.pos[0] + move * self.speed, self.pos[1])
